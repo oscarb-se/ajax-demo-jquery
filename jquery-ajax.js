0598281615922fcadf8bf78d944a42e57e574403
oscarb-se <github@oscarb.se>
2016-05-04 11:21:33 +0200

    Add function for when request fails

--- jquery-ajax.js
+++ jquery-ajax.js
@@ -17,10 +17,11 @@ $('#my-button').click(function() {
   $.get("https://api.github.com/zen", function(data, textStatus, jqXHR) {
     // Vilken status?
     console.log(textStatus);
+    console.log(jqXHR);
     
     // Visa texten från servern i stycket (<p>) med id:t show-server-text
     $('#show-server-text').text(data);
     
-  } )
+  } ).fail(function() { console.log("Request gav fel!")} )
 } 
 );
