02d7a1501e3df0e0124daa4509c4768642f7bef5
oscarb-se <github@oscarb.se>
2016-05-04 10:43:41 +0200

    Add function that fetches server data

--- jquery-ajax.js
+++ jquery-ajax.js
@@ -2,4 +2,18 @@
 
 // Använd jQuery och AJAX
 
-$('#my-button').click(function() { console.log("Knappen klickades på!") } );
+$('#my-button').click(function() { 
+  console.log("Knappen klickades på!") 
+  
+  // Gör en GET request med AJAX i jQuery
+  $.get("https://api.github.com/zen", function(data, textStatus, jqXHR) {
+    // Vilken status?
+    console.log(textStatus);
+    
+    // Visa texten från servern i stycket (<p>) med id:t show-server-text
+    $('#show-server-text').text(data);
+    
+    
+  } )
+} 
+);
